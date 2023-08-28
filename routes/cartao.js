1f08263d1158a693b9df177aeb51d2cadcc86e00
jaoviitor <joaoviiitorvieira@gmail.com>
2023-08-28 10:12:15 -0300

    add rota cartao

---
+++ routes/cartao.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+
+//CADASTRA UM CARTÃO
+router.post('/cadastro', (req, res, next) => {
+    mysql.getConnection((error, conn) =>{
+        if(error){ return res.status(500).send({ error: error }) };
+        conn.query(`INSERT INTO cartao (cliente_id, token) VALUES (?, ?)`,
+        [req.body.cliente_id ,req.body.token],
+        (error, results) =>{
+            conn.release();
+            if (error) { return res.status(500).send({ error: error })}
+            res.status(201).send({ mensagem: 'Cartão enviado!', Id: results.insertId })
+        })
+    })
+});
